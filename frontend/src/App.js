c3115c672520c74750f6cff1db1953e75a00feb4
sharbel93 <sharbel@techfactory.ltd>
2019-11-27 12:33:45 +0300

    First react component - adding a Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,22 @@ import './App.css';
 import AuthPage from "./pages/Auth";
 import EventsPage from "./pages/Events";
 import BookingsPage from "./pages/Bookings";
-
+import MainNavigation from './components/Navigation/MainNavigation';
 
 
 function App() {
   return (
     <BrowserRouter>
-        <Switch>
-            <Redirect from="/" to="/auth" exact />
-            <Route path="/auth" component={AuthPage} />
-            <Route path="/events" component={EventsPage} />
-            <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <MainNavigation />
+        <main className="main-content">
+            <Switch>
+                <Redirect from="/" to="/auth" exact />
+                <Route path="/auth" component={AuthPage} />
+                <Route path="/events" component={EventsPage} />
+                <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+        </main>
+
     </BrowserRouter>
   );
 }
