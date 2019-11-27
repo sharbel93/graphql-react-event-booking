f6f35d47fb93a591c688124f7eef1a1ffe757136
sharbel93 <sharbel@techfactory.ltd>
2019-11-27 14:17:19 +0300

    implementing React Auth, by hiiting the API

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+
+// middleware
+app.use((req, res, next) => {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+}
+next();
+});
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({
@@ -19,7 +31,7 @@ schema: graphQlSchema,
 }));
 
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true }).then(() => {
-    app.listen(3000);
+    app.listen(3100);
 }).catch( err => {
    console.log(err);
 });
